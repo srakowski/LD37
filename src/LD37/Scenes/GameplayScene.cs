c8579d7b8dbd247f884ee9292f49db76512d4c51
Shawn Rakowski <srakowski@live.com>
2016-12-09 23:21:46 -0600

    working boilerplate

--- src/LD37/Scenes/GameplayScene.cs
+++ src/LD37/Scenes/GameplayScene.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace LD37.Scenes
 {
     public class GameplayScene : ReflectiveSceneBuilder
     {
+        public override Color BackgroundColor => Color.Black;
     }
 }
