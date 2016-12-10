7a2b36b0e6b3a139c6b5e500e30461129e5ce3ff
Shawn Rakowski <srakowski@live.com>
2016-12-09 23:13:00 -0600

    added coldsteel as submodule, working on more boilerplate

---
+++ src/LD37/Scenes/GameplayScene.cs
@@ -0,0 +1,11 @@
+﻿using Coldsteel.Composition;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LD37.Scenes
+{
+    public class GameplayScene : ReflectiveSceneBuilder
+    {
+    }
+}
